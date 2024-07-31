035e80bd96c05567d5a69206f3abd7d4e1607cf9
Tejaswi Rastogi <tejaswi376@gmail.com>
2024-07-31 15:19:51 +0530

    "Learned about attributes and tags in Html" "In JS did learn about PreDecrement and PreIncrement"

---
+++ JavaScript/Day_3_Token/operator.js
@@ -0,0 +1,15 @@
+/* Logical Not ! */
+
+
+console.log(!false)
+console.log(!true)
+
+
+
+console.log(!0)
+console.log(!1)
+
+console.log(!0)
+console.log(!0.5)
+console.log(!-1)
+
---
+++ JavaScript/Day_3_Token/preDecrement.js
@@ -0,0 +1,51 @@
+/ * Predecrement operator (--) */
+
+
+// let a = 5;
+//     a = --a;
+//     a = --a;
+//     a = --a;
+
+//     console.log(a)
+
+// let a = 10 ;
+// a = --a + ++a;
+// let b = (++a*2 + --a*2) / (++a*2)
+// b = ++b;
+// console.log(a)
+// console.log(b)
+
+let a = 5;
+a = ++a*3 + --a*2 - ++a*2
+a=(++a*3/2) + (--a*2/3)
+let b = (++a + -
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    -a)*3/2
+b = (++b*2 - --b*2 + ++b*2)/(++a + --a)
+console.log(a)
+console.log(b)
